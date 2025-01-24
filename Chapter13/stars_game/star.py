3c1f4e121534108b2a82a98c48099fdef048a9c2
Diego Dominguez <shawndiego15@gmail.com>
2025-01-23 18:59:30 -0600

    Add files via upload

---
+++ Chapter13/stars_game/star.py
@@ -0,0 +1,23 @@
+# Star for Star Game
+
+import pygame
+from pygame.sprite import Sprite
+
+class Star(Sprite):
+    """A class to represent a single star in the sky."""
+
+    def __init__(self, sg_game):
+        """Initialize the star and set its starting position."""
+        super().__init__()
+        self.screen = sg_game.screen
+
+        # Load the star image and set its rect attribute.
+        self.image = pygame.image.load('chapter13_exercises/images/star.png')
+        self.rect = self.image.get_rect()
+
+        # Start each new star near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store the star's exact vertical position.
+        self.y = float(self.rect.y)
